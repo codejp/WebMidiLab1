87e7e423b25a4a2c286413fc0a401f9172254269
yasuaki.matsuda <dark_crash@hotmail.com>
2016-08-13 09:46:03 +0900

    modify Zundoko include buffered

--- WebMidiLab1/Hubs/WebMIDIHubBuffered.cs
+++ WebMidiLab1/Hubs/WebMIDIHubBuffered.cs
@@ -10,16 +10,7 @@ namespace WebMidiLab1.Hubs
             Clients.All.WebMIDIInput(status, msg1, msg2, DateTime.Now.Ticks);
         }
 
-        private static DateTime d1970 = new DateTime(1970, 1, 1, 0, 0, 0);
-        private static long tick1970 = d1970.Ticks;
-        private static TimeSpan timespan1970 = TimeSpan.FromTicks(d1970.Ticks);
 
-        public void WebMIDISync(long tick)
-        {
-            var client = TimeSpan.FromMilliseconds(tick).Add(timespan1970);
-            var server = TimeSpan.FromTicks(DateTimeOffset.Now.UtcTicks);
-            Clients.Caller.WebMIDIInputSync(server.Ticks - client.Ticks);
-        }
         public void WebMIDISyncThrough(long tick)
         {
             Clients.Caller.WebMIDIInputSync(tick);
